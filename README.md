8f77551e3d54332d95c86f04131a47a7cea6c71a
Hrvoje Kusulja <hkusulja@3pro.eu>
2017-08-03 14:45:08 +0200

    Initial SetVersion.ps1 Update README docs

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# SetVersion
-Sets the project version number at build time
+# SetVersion script for updating project build versions
+SetVersion is a PowerShell script which enables you to dynamically change your project build version number while using it in your autobuild process.
+It is suitable for .NET Core projects (.csproj and .nuspec) and FrontEnd projects (JavaScript .js or TypeScript .ts).
+It is intended to be used as a Task in your build definition. Example, as Powershell Tasks in your Visual Studio Team Services build definition.
+
+# Usage
+You have to add your script to your project code repository which will be used in your build definition.
+Next step is to add a new task in your build definition to call SetVersion.ps1 script before project build task.
+`Note: In JavaScript and TypeScript files it searches for string "--version--" which will be replaced by the BuildNumber/version argument` 
+
+# SetVersion.ps1 Arguments
+SetVersion.ps1 script has 3 required parameters.
+```sh
+./SetVersion.ps1 -buildNumber BuildNumber -type Type -filePath filePath
+```
+BuildNumber - version number you wish to set for your project
+Type - type of your project (csproj|nuspec|js|ts)
+filePath - path to file which contains your project version
+
+### Basics and Examples
+In case of Visual Studio Team Services, PowerShell tasks which calls SetVersion.ps1 script, you will commonuse Visual Studio Team Services variables
+```sh
+$(Build.BuildNumber)
+```
+This is an environmental variable which exists by default in your build procedure, it can be fine tuned in procedure's options under the 'Build number format' setting. If you leave that setting empty, your build number will increment, and will be in an integer format so it will work. `Note: SetVersion script expects the buildNumber parameter to be an integer!` 
+```sh
+$(Build.SourcesDirectory)
+```
+This is an environmental variable which exists by default in your build procedure and it is used to position correctly on your build machine when the build is running.
+
+#### Example for .NET Core project which has to change build number only in csproj file
+```sh
+-buildNumber $(Build.BuildNumber) -type csproj -filePath $(Build.SourcesDirectory)\src\ServerBackEnd\ServerBackEnd.csproj
+```
+#### Example for .NET Core project which has to change build number only in nuspec file
+```sh
+-buildNumber $(Build.BuildNumber) -type nuspec -filePath $(Build.SourcesDirectory)\src\ServerBackEnd\ServerBackEnd.nuspec
+```
+#### Example for .NET Core project which has to change build number in both csproj and nuspec files in the same script call
+```sh
+-buildNumber $(Build.BuildNumber) -type csprojnuspec -filePath $(Build.SourcesDirectory)\src\ServerBackEnd\ServerBackEnd.csproj
+``` 
+`Previous example assumes that nuspec and csproj files are placed in the same directory and have the same names (except extensions).` Under filePath paremeter you can point to whatever of those files you want. It is also important to emphasize that the versions will not be synced in this step. Still every of those files has it's own major, minor and maintenance numbers which will be left intact, only the revisionNumber will be the 100% same and set to the value you've passed to -buildNumber parameter.
+#### Example for JavaScript project which has to change build number in an index.constants.js file
+```sh
+-buildNumber $(Build.BuildNumber) -type js -filePath $(Build.SourcesDirectory)\src\Portal\src\app\index.constants.js
+```
+`Note: SetVersion script expects the index.constants.js file to have a substring '--version--' (without quotes) which will be replaced to a value you’ve passed to -buildNumber parameter.`
+If your version is in .ts file then you'll use the following example:
+```sh
+-buildNumber $(Build.BuildNumber) -type ts -filePath $(Build.SourcesDirectory)\src\Portal\src\app\index.constants.ts
+```
---
+++ SetVersion.ps1
@@ -0,0 +1,116 @@
+﻿# SetVersion script for updating project build versions
+
+# all parameters are required
+# -buildNumber - the number which will be set as a build number in your version, i.e. 13
+# -filePath - path to the file in which we want to change the build version
+# -type - type of the file which will be changed
+param (
+    [int]$buildNumber = $(throw "-buildNumber is required."), # the build version, from VSTS build i.e. "974"
+    [string]$filePath = $(throw "-filePath is required."), #$PSScriptRoot, # path to the file i.e. 'C:\Users\ben\Code\csproj powershell\MySmallLibrary.csproj'
+    [string]$type = $(throw "-type is required. |csproj|nuspec|csprojnuspec|js|ts|") # path to the file i.e. 'C:\Users\ben\Code\csproj powershell\MySmallLibrary.nuspec'
+)
+
+# function which works with both nuspec and csproj files, reads the current version from the file and then replaces only 
+# the part of the version (revisionNumber) with the -buildNumber you've specified
+function SetCsprojNuspecBuildVersion ([string]$currentFileType, [string]$currentFilePath)
+{
+    Write-Host "Starting process of generating new version number for the "$type
+    Write-Host "New Build: "$buildNumber
+    $xml=New-Object XML
+    $xml.Load($currentFilePath)
+
+    [string]$oldBuildString="";
+    # propertyToSet is a switch/indexer (depending on a type of csproj) for the property which has to be replaced
+    $propertyToSet=-1; # csproj - only one Property group element in csproj so version property path is defined
+    if($currentFileType -eq 'csproj')
+    {
+        if($xml.Project.PropertyGroup -isnot [array] -or $xml.Project.PropertyGroup.count -le 1)
+        {
+            $oldBuildString = $xml.Project.PropertyGroup.Version
+        }
+        elseif($xml.Project.PropertyGroup.count -gt 1)
+        {
+            $propertyGroups = $xml.Project.PropertyGroup
+            $myBuildNumber = "";
+            foreach ($currentPropertyGroup in $propertyGroups)
+            {
+                $propertyToSet++; # csproj - multiple Property group elements in csproj so version property path is dynamic and the variable is incremented to point to correct index
+	            if($currentPropertyGroup.Version)
+                {
+		            $oldBuildString = $currentPropertyGroup.Version;
+		            break;
+                }
+            }
+        }
+        else
+        {
+            $(throw "Cannot find version property in csproj file: $filePath");
+        }
+    }
+    elseif($currentFileType -eq 'nuspec')
+    {
+        $oldBuildString = $xml.package.metadata.version
+        $propertyToSet=-2; # nuspec - defined version property path
+    }
+    
+    Write-Host "Current "$currentFileType" version: "$oldBuildString
+    # split the current version to subnumbers
+    $oldSplitNumber = $oldBuildString.Split(".")
+    # define the new version subnumber properties
+    $majorNumber = $oldSplitNumber[0]
+    $minorNumber = $oldSplitNumber[1]
+    $maintenanceNumber = $oldSplitNumber[2]
+    $revisionNumber = $buildNumber
+    # define the new version
+    $myBuildNumber = $majorNumber + "." + $minorNumber + "." + $maintenanceNumber + "." + $revisionNumber
+    
+    # set the new version property
+    # nuspec
+    if($propertyToSet -eq -2)
+        {$xml.package.metadata.version = $myBuildNumber;}
+    # csproj - one PropertyGroup element
+    elseif($propertyToSet -eq -1)
+        {$xml.Project.PropertyGroup.Version=$myBuildNumber;}
+    # csproj - multiple PropertyGroup elements
+    else
+        {$xml.Project.PropertyGroup[$propertyToSet].Version=$myBuildNumber;}
+    # save the xml file
+    $xml.Save($currentFilePath)
+
+    Write-Host "Updated "$currentFilePath" and set build to version: "$myBuildNumber
+}
+
+# function which works best for frontend projects where you actually can replace some placeholder text into the build number
+# placeholder text which your file should contain is: '--version--' (without quotes)
+function SetJSTSBuildVersion ([string]$currentFileType, [string]$currentFilePath)
+{
+    Write-Host "Updating build version constant to "$buildNumber
+    (Get-Content $currentFilePath).replace('--version--', $buildNumber) | Set-Content $currentFilePath
+    Write-Host "Updated "$type" file "$currentFilePath" and set build version to "$buildNumber
+}
+
+#script logic execution
+if($type -eq 'csprojnuspec')
+{
+    Write-Host "type: csprojnuspec";
+    #ensuring first file is csproj
+    $filePath = $filePath.replace('.nuspec','.csproj')
+    SetCsprojNuspecBuildVersion 'csproj' $filePath;
+    #ensuring second file is nuspec
+    $secondFilePath = $filePath.replace('.csproj','.nuspec')
+    SetCsprojNuspecBuildVersion 'nuspec' $secondFilePath;
+}
+elseif ($type -eq 'csproj' -or $type -eq 'nuspec')
+{
+    Write-Host "type: "$type;
+    SetCsprojNuspecBuildVersion $type $filePath;
+}
+elseif ($type -eq 'js' -or $type -eq 'ts')
+{
+    Write-Host "type: "$type;
+    SetJSTSBuildVersion $type $filePath;
+}
+else
+{
+    $(throw "Unknown -type parameter. |csproj|nuspec|csprojnuspec|js|ts|");
+}
